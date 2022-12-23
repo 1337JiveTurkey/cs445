a1d8672446f5ca654154a8645a422920f48a19a1
1337JiveTurkey <1337JiveTurkey@gmail.com>
2022-12-23 01:39:16 -0500

    More woods.

--- src/main/java/edu/semo/cs445/mvc/model/treasure/Wood.java
+++ src/main/java/edu/semo/cs445/mvc/model/treasure/Wood.java
@@ -3,15 +3,46 @@ package edu.semo.cs445.mvc.model.treasure;
 import edu.semo.cs445.mvc.model.RandGenEnum;
 
 public enum Wood implements Material {
+	Acacia,
+	Alder,
 	Ash,
+	Aspen,
+	Balsa,
+	Basswood,
+	Birch,
+	Boxwood,
+	Buckeye,
+	Camphor,
+	Cedar,
+	Cherry,
+	Chestnut,
+	Cottonwood,
+	Cypress,
+	Dogwood,
 	Ebony,
 	Elm,
+	Eucalyptus,
+	Fir,
+	Greenheart,
+	Gum,
+	Hemlock,
+	Hickory,
+	Ironwood,
+	Larch,
+	Locust,
 	Mahogany,
 	Maple,
 	Oak,
+	Olive,
+	Pine,
+	Poplar,
 	Purpleheart,
 	Rosewood,
-	Walnut;
+	Sandalwood,
+	Spruce,
+	Teak,
+	Walnut,
+	Willow;
 
 	public static RandGenEnum<Wood> Random = new RandGenEnum<>(Wood.class);
 }
